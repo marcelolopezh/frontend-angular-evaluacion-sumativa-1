2870aaddb4f375a279b345f424f9bf3f16a3cd3c
Marcelo López <marcelolopezhernandez@gmail.com>
2021-09-24 14:32:11 -0300

    Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AsideComponent,
+    CategoriasComponent,
+    ProductosComponent,
+    LoginComponent,
+    RegistroComponent,
+    CarruselComponent,
+    BuscadorComponent,
+
   ],
   imports: [
     BrowserModule
